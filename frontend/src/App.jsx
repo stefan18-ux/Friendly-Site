d5bb851fda1d4a929cb07dfc57ab303e83f6f21c
Oprea Stefan Antoniu <sopreaa@gmail.com>
2024-07-29 22:35:16 +0300

    frontend almost done

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import {Button, Container, Stack} from '@chakra-ui/react'
+import Navbar from "./components/Navbar"
+
+function App() {
+  return (
+    <Stack minH={"100vh"}>
+      <Navbar />
+      <Container maxW={"1200px"} my={4}>
+      </Container>
+    </Stack>
+  )
+}
+
+export default App
