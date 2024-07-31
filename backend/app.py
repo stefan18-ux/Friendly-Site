78dd843bd1b6a6d63625b28a93c39272781d0dba
Oprea Stefan Antoniu <sopreaa@gmail.com>
2024-07-31 17:21:10 +0300

    all done

--- backend/app.py
+++ backend/app.py
@@ -12,6 +12,16 @@ app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db = SQLAlchemy(app)
 
+frontend_folder = os.path.join(os.getcwd(),"..", "frontend")
+dist_folder = os.path.join(frontend_folder, "dist")
+
+@app.route("/", defaults={"filename":""})
+@app.route("/<path:filename>")
+def index(filename):
+    if not filename:
+      filename = "index.html"
+    return send_from_directory(dist_folder,filename)
+
 # api routes
 import routes
 
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,36 @@
-import {Button, Container, Stack} from '@chakra-ui/react'
-import Navbar from "./components/Navbar"
+import { Container, Stack, Text } from "@chakra-ui/react";
+import Navbar from "./components/Navbar";
+import UserGrid from "./components/UserGrid";
+import { useState } from "react";
+
+export const BASE_URL = "http://127.0.0.1:5000/api";
 
 function App() {
-  return (
-    <Stack minH={"100vh"}>
-      <Navbar />
-      <Container maxW={"1200px"} my={4}>
-      </Container>
-    </Stack>
-  )
+	const [users, setUsers] = useState([]);
+
+	return (
+		<Stack minH={"100vh"}>
+			<Navbar setUsers={setUsers} />
+
+			<Container maxW={"1200px"} my={4}>
+				<Text
+					fontSize={{ base: "3xl", md: "50" }}
+					fontWeight={"bold"}
+					letterSpacing={"2px"}
+					textTransform={"uppercase"}
+					textAlign={"center"}
+					mb={8}
+				>
+					<Text as={"span"} bgGradient={"linear(to-r, cyan.400, blue.500)"} bgClip={"text"}>
+						My Besties
+					</Text>
+					🚀
+				</Text>
+
+				<UserGrid users={users} setUsers={setUsers} />
+			</Container>
+		</Stack>
+	);
 }
 
-export default App
+export default App;
